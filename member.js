533db63af8b3d236551870a27e072dc2151ac0f1
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-10-17 04:19:00 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,13 +1,12 @@
 function skillMember() {
-    return {
-      restrict: "E",
-      templateUrl: "modules/skills/view/member.html",
-      controller: "SkillsMemberController",
-      controllerAs: "vm",
-      bindToController:true,
-      scope: {
-        member: "=",
-      },
-    }
+  return {
+    restrict: 'E',
+    templateUrl: 'modules/skills/views/member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'vm',
+    bindToController: true,
+    scope: {
+      member: '=',
+    },
   }
-  
+}
